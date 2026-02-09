12d6dfd26fcf8862cf6d746e4cda521296c794da
Dhairyashil Shinde <your-github-email@domain.com>
2026-02-09 12:23:30 +0530

    Add debug logs for production diagnosis

---
+++ scripts/Untitled-1.py
@@ -0,0 +1,15 @@
+seq = ["soap","cat","dog","prathmest"]
+
+for i in seq:
+
+    for j in i:
+
+        j = i.split()
+
+        if j == "s":
+
+            j.remove()
+
+    return()
+
+print("done")
